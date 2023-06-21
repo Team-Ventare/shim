7e78f00d29dadaf897a25de78a452becc7a86147
Nicholas Moreland <nmoreland18@outlook.com>
2023-06-21 11:55:40 -0400

    home page link for mobile

--- app/nav/navbar.tsx
+++ app/nav/navbar.tsx
@@ -70,7 +70,7 @@ const Nav = () => {
         <div className="fixed inset-0 z-50" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <Link href="#" className="-m-1.5 p-1.5">
+            <Link href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <img
                 className="h-8 w-auto"
