9d1bdacd33a3b3a28ced772f8d11b4a6ed0dca40
Nicholas Moreland <nmoreland18@outlook.com>
2023-06-21 14:02:41 -0400

    inventory mockup additions

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -3,6 +3,7 @@
 import { CiShoppingCart } from "react-icons/ci";
 import { TiLocation } from "react-icons/ti";
 import { FaTruck } from "react-icons/fa";
+import { IoAdd } from "react-icons/io5";
 import { Nav } from "../nav/navbar";
 
 const products = [
@@ -16,8 +17,23 @@ export default function Example() {
     <div className="bg-white min-h-screen min-w-screen max-w-screen">
       <Nav />
 
+      <div className="flex justify-between items-center p-4 border-b">
+        <div>
+          <button className="text-sm font-semibold leading-6 text-slate-700 hover:bg-slate-100 px-3 py-2 rounded-md">
+            Notifications
+          </button>
+          <button className="text-sm font-semibold leading-6 text-slate-700 hover:bg-slate-100 px-3 py-2 rounded-md ml-1">
+            Recent Scans
+          </button>
+        </div>
+        <button className="text-sm font-semibold leading-6 text-slate-700 hover:bg-slate-100 px-3 py-2 rounded-md flex items-center">
+          <IoAdd className="text-lg mr-1" />
+          <span>Add Item</span>
+        </button>
+      </div>
+
       <div className="mx-auto min-w-screen p-4">
-        <div className="text-center p-4 mb-2">
+        <div className="text-center p-4">
           <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
             Inventory
           </h2>
