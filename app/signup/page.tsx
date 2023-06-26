754523e3645277feba519eff069958bbba9c4766
Nicholas Moreland <nmoreland18@outlook.com>
2023-06-26 17:26:49 -0400

    password ui updates

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { BiLockAlt, BiLockOpenAlt } from "react-icons/bi";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../lib/firebase";
 import { Nav } from "../nav/navbar";
@@ -15,8 +16,7 @@ import {
 export default function SignUp() {
   const { theme, setTheme } = useTheme();
   const [user] = useAuthState(auth);
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConfirm, setPasswordConfirm] = useState("");
@@ -35,7 +35,7 @@ export default function SignUp() {
       );
       const user = result.user;
       await updateProfile(user, {
-        displayName: firstName + " " + lastName,
+        displayName: name,
       });
       await sendEmailVerification(user);
       alert("Account created successfully. Please verify your email.");
@@ -64,34 +64,21 @@ export default function SignUp() {
             <div className="grid gap-6 mb-6">
               <div>
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  First name
+                  Full Name
                 </label>
                 <input
                   type="text"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="John"
+                  placeholder="John Doe"
                   required
-                  value={firstName}
-                  onChange={(e) => setFirstName(e.target.value)}
-                />
-              </div>
-              <div>
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  Last name
-                </label>
-                <input
-                  type="text"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Doe"
-                  required
-                  value={lastName}
-                  onChange={(e) => setLastName(e.target.value)}
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                 />
               </div>
             </div>
             <div className="mb-6">
               <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                Your Email
+                Email
               </label>
               <div className="relative">
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -116,31 +103,151 @@ export default function SignUp() {
                 />
               </div>
             </div>
+            {/* Password Field */}
             <div className="mb-6">
-              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                Password
-              </label>
-              <input
-                type="password"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="•••••••••"
-                required
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
+              {password.length == 0 ? (
+                <>
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Password
+                  </label>
+                  <div className="relative">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                      <BiLockOpenAlt className="w-5 h-5 text-gray-500 dark:text-gray-400" />
+                    </div>
+                    <input
+                      type="password"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="•••••••••"
+                      required
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                </>
+              ) : (
+                <>
+                  {password.length >= 6 ? (
+                    <>
+                      <label className="block mb-2 text-sm font-medium text-green-700 dark:text-green-500">
+                        Password
+                      </label>
+                      <div className="relative">
+                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                          <BiLockAlt className="w-5 h-5 text-green-500 dark:text-green-400" />
+                        </div>
+                        <input
+                          type="password"
+                          className="bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-green-500"
+                          placeholder="•••••••••"
+                          required
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                        />
+                      </div>
+                      <p className="mt-2 text-sm text-green-600 dark:text-green-500">
+                        <span className="font-medium">Well done! </span>
+                        Your password meets our requirements.
+                      </p>
+                    </>
+                  ) : (
+                    <>
+                      <label className="block mb-2 text-sm font-medium text-red-700 dark:text-red-500">
+                        Password
+                      </label>
+                      <div className="relative">
+                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                          <BiLockOpenAlt className="w-5 h-5 text-red-500 dark:text-red-400" />
+                        </div>
+                        <input
+                          type="password"
+                          className="bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full pl-10 p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500"
+                          placeholder="•••••••••"
+                          required
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                        />
+                      </div>
+                      <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                        <span className="font-medium">Oh, snapp! </span>
+                        Your password must be atleast six characters.
+                      </p>
+                    </>
+                  )}
+                </>
+              )}
             </div>
+            {/* Password Confirm Field */}
             <div className="mb-6">
-              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                Confirm password
-              </label>
-              <input
-                type="password"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="•••••••••"
-                required
-                value={passwordConfirm}
-                onChange={(e) => setPasswordConfirm(e.target.value)}
-              />
+              {passwordConfirm.length == 0 ? (
+                <>
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Confirm Password
+                  </label>
+                  <div className="relative">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                      <BiLockOpenAlt className="w-5 h-5 text-gray-500 dark:text-gray-400" />
+                    </div>
+                    <input
+                      type="password"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="•••••••••"
+                      required
+                      value={passwordConfirm}
+                      onChange={(e) => setPasswordConfirm(e.target.value)}
+                    />
+                  </div>
+                </>
+              ) : (
+                <>
+                  {password === passwordConfirm ? (
+                    <>
+                      <label className="block mb-2 text-sm font-medium text-green-700 dark:text-green-500">
+                        Confirm Password
+                      </label>
+                      <div className="relative">
+                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                          <BiLockAlt className="w-5 h-5 text-green-500 dark:text-green-400" />
+                        </div>
+                        <input
+                          type="password"
+                          className="bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-green-500"
+                          placeholder="•••••••••"
+                          required
+                          value={passwordConfirm}
+                          onChange={(e) => setPasswordConfirm(e.target.value)}
+                        />
+                      </div>
+                      <p className="mt-2 text-sm text-green-600 dark:text-green-500">
+                        <span className="font-medium">Well done! </span>
+                        Both passwords match our requirements.
+                      </p>
+                    </>
+                  ) : (
+                    <>
+                      <label className="block mb-2 text-sm font-medium text-red-700 dark:text-red-500">
+                        Confirm Password
+                      </label>
+                      <div className="relative">
+                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                          <BiLockOpenAlt className="w-5 h-5 text-red-500 dark:text-red-400" />
+                        </div>
+                        <input
+                          type="password"
+                          className="bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full pl-10 p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500"
+                          placeholder="•••••••••"
+                          required
+                          value={passwordConfirm}
+                          onChange={(e) => setPasswordConfirm(e.target.value)}
+                        />
+                      </div>
+                      <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                        <span className="font-medium">Oh, snapp! </span>
+                        Both passwords must match.
+                      </p>
+                    </>
+                  )}
+                </>
+              )}
             </div>
             <button
               type="submit"
