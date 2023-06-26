8a8b63f673afb2a9a9e67d09303cf589990ea147
Henry <115604958+kniatos1337@users.noreply.github.com>
2023-06-25 23:33:03 -0500

    [Update] Home/Nav -updated text info -added shim logo -added dark/light theme
    -added logo to nav

--- app/nav/navbar.tsx
+++ app/nav/navbar.tsx
@@ -34,8 +34,8 @@ const Nav = () => {
             <Link href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                className="h-12 w-auto"
+                src="/logo1.png" 
                 alt=""
               />
             </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,11 @@ import { auth } from "./lib/firebase"; //
 import { Nav } from "./nav/navbar"; //
 import { RxCross2 } from "react-icons/rx"; //
 import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
 
 export default function Home() {
   const [showWarningModal, setShowWarningModal] = useState(true);
+  const { theme, setTheme } = useTheme();
   const [user] = useAuthState(auth); //add
 
   useEffect(() => {
@@ -16,9 +18,10 @@ export default function Home() {
   }, [user, showWarningModal]);
 
   return (
-    <div className="bg-white min-h-screen min-w-screen max-w-screen">
+    <div className={theme == "dark" ? "dark" : undefined}>
+    <div className="min-h-screen min-w-screen max-w-screen bg-white dark:bg-slate-900">
+      
       <Nav />
-
       {!user && showWarningModal ? undefined : (
         <div className="bg-yellow-50 border-y border-yellow-400 p-4">
           <div className="flex justify-between">
@@ -34,8 +37,11 @@ export default function Home() {
           </div>
         </div>
       )}
-
-      <div className="mx-auto min-w-screen max-w-2xl py-32 sm:py-48 px-6 pt-14 lg:px-8">
+      <div style={{
+        display: "flex",
+        justifyContent: "center",
+      }} className="py-6"><img src="/scaledshimlogo.png" width={520} height={77} alt=""/></div>
+      <div className="mx-auto min-w-screen max-w-2xl py-32 sm:py-0 px-6 pt-14 lg:px-8">
         <div className="hidden sm:mb-8 sm:flex sm:justify-center">
           <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
             Announcing our newest update.{" "}
@@ -46,10 +52,10 @@ export default function Home() {
           </div>
         </div>
         <div className="text-center">
-          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl py-12 dark:text-white">
             Smart Hospital Inventory Management.
           </h1>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
+          <p className="mt-0 text-lg leading-8 text-gray-600">
             Developed by Ventare, a team of computer science and software engineers at the University of Texas at Arlington.
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
@@ -62,13 +68,14 @@ export default function Home() {
             </a>
             <a
               href="#"
-              className="text-sm font-semibold leading-6 text-gray-900"
+              className="text-sm font-semibold leading-6 text-gray-900 dark:text-white"
             >
               Learn more <span aria-hidden="true">→</span>
             </a>
           </div>
         </div>
       </div>
+          </div>
     </div>
   );
 }
