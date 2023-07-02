7f788cda3e1f7b3d3ea8d6366f57074c58a03554
Nicholas Moreland <nmoreland18@outlook.com>
2023-07-02 15:00:29 -0400

    add inventory item api route

--- app/api/inventory/route.tsx
+++ app/api/inventory/route.tsx
@@ -6,3 +6,14 @@ export async function GET(request: Request) {
   const inventory = await prisma.inventory.findMany();
   return NextResponse.json(inventory);
 }
+
+// Add Inventory Item (/api/inventory/)
+export async function POST(request: Request) {
+  const json = await request.json();
+
+  const created = await prisma.inventory.create({
+    data: json,
+  });
+
+  return new NextResponse(JSON.stringify(created), { status: 201 });
+}
