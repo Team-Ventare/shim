b9e918c50b05b59fc0baeba8ecdf08766b1881b4
nicholasmoreland <nmoreland18@outlook.com>
2023-07-09 21:56:19 -0500

    fix navbar color

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -15,7 +15,7 @@ export async function NavBar() {
   const user = await getUserSession();
 
   return (
-    <div className="top-0 z-0 sticky mx-auto py-1 border-b">
+    <div className="top-0 z-0 sticky mx-auto py-1 border-b bg-white">
       <div className="flex items-center py-2 px-8 space-x-4">
         <Link href="/" className="-m-1.5">
           <Image height={46} width={46} src="/logo1.png" alt="" />
