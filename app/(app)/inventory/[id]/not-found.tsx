3cd7446856d1d222f82f03ff34bff9550630a473
nicholasmoreland <nmoreland18@outlook.com>
2023-07-09 23:59:01 -0500

    product page push

---
+++ app/(app)/inventory/[id]/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function ProjectNotFound() {
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find that product.</p>
+      <p>
+        View <Link href="/inventory">all products.</Link>.
+      </p>
+    </div>
+  );
+}
