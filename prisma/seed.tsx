dd20c02ea7868dd6ed98dcd222d4f3eb5bd901d9
Nicholas Moreland <nmoreland18@outlook.com>
2023-07-10 14:46:06 -0400

    update to db schema

--- prisma/seed.tsx
+++ prisma/seed.tsx
@@ -37,44 +37,63 @@ const inventory = [
     amount: 100,
     location: "A1",
     type: "CONSMABLE_SUPPLIES",
+    checkedOut: {},
   },
   {
-    name: "Niltrate 20mg",
+    name: "Nursing Bed",
     amount: 100,
     location: "A1",
     type: "CONSMABLE_SUPPLIES",
+    checkedOut: {},
   },
   {
     name: "Dell Computer",
     amount: 10,
     location: "A1",
     type: "COMPUTERS",
+    checkedOut: {},
+  },
+];
+
+const purchaseRequest = [
+  {
+    userId: "ce9d9155-0d37-44cd-a967-9b34822b55d9",
+    name: "Latex Gloves",
+    category: "CONSMABLE_SUPPLIES",
+    price: 50,
   },
   {
-    name: "IV Arm",
-    amount: 10,
-    location: "A1",
-    type: "SIMULATION_EQUIPMENT",
-    status: "CHECKED_OUT",
+    userId: "9fa1ff90-7b45-4b7a-a441-f20f549550e5",
+    name: "Neddles",
+    category: "CONSMABLE_SUPPLIES",
+    price: 100,
+    status: "APPROVED",
   },
   {
-    name: "Beds",
-    amount: 10,
-    location: "B2",
-    type: "MEDICAL_FURNITURE",
-    status: "CHECKED_OUT",
+    userId: "101fe5a6-f439-4a66-b382-77aef57a3ab0",
+    name: "Scrubs",
+    category: "OTHER",
+    price: 300,
+    status: "REJECTED",
+  },
+];
+
+const suppliers = [
+  {
+    name: "Amazon",
+    address: "123 Amazon Way",
+    phone: "123-456-7890",
+    email: "order@amazon.com",
+    website: "https://www.amazon.com",
+    notes: "",
   },
   {
-    name: "Surgical Tools",
-    amount: 10,
-    location: "B2",
-    type: "NONCONSUMABLE_SUPPLIES",
-  },
-  {
-    name: "Surgical Gloves",
-    amount: 100,
-    location: "B2",
-    type: "CONSMABLE_SUPPLIES",
+    name: "Walmart",
+    address: "123 Walmart Way",
+    phone: "123-456-7890",
+    email: "order@walmart.com",
+    website: "https://www.walmart.com",
+    notes: "",
   },
 ];
 
@@ -85,7 +104,11 @@ async function main() {
   for (const u of users) {
     const user = await prisma.user.upsert({
       where: { email: u.email },
-      update: {},
+      update: {
+        name: u.name,
+        role: u.role,
+        password: password,
+      },
       create: {
         email: u.email,
         name: u.name,
@@ -103,10 +126,38 @@ async function main() {
         amount: i.amount,
         location: i.location,
         type: i.type,
-        status: i.status,
       },
     });
     console.log(`Created item with id: ${item.id}`);
+  }
+
+  for (const p of purchaseRequest) {
+    const purchase = await prisma.purchaseRequest.create({
+      data: {
+        name: p.name,
+        category: p.category,
+        price: p.price,
+        status: p.status,
+        user: {
+          connect: { id: p.userId },
+        },
+      },
+    });
+    console.log(`Created purchase request with id: ${purchase.id}`);
+  }
+
+  for (const s of suppliers) {
+    const supplier = await prisma.suppliers.create({
+      data: {
+        name: s.name,
+        address: s.address,
+        phone: s.phone,
+        email: s.email,
+        website: s.website,
+        notes: s.notes,
+      },
+    });
+    console.log(`Created supplier with id: ${supplier.id}`);
   }
 
   console.log(`Seeding finished.`);
