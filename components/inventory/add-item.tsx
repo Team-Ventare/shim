3aa51af60cb971aea84f3dc5e4f455782e0d46e4
Nicholas Moreland <nmoreland18@outlook.com>
2023-07-11 16:46:30 -0400

    ui for add item

--- components/inventory/add-item.tsx
+++ components/inventory/add-item.tsx
@@ -12,6 +12,14 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { Plus } from "lucide-react";
+import {
+  Select,
+  SelectItem,
+  SelectValue,
+  SelectContent,
+  SelectTrigger,
+  SelectLabel,
+} from "@/components/ui/select";
 
 export default function AddItemSheet() {
   return (
@@ -28,28 +36,85 @@ export default function AddItemSheet() {
       </SheetTrigger>
       <SheetContent>
         <SheetHeader>
-          <SheetTitle>Edit profile</SheetTitle>
+          <SheetTitle>New product</SheetTitle>
           <SheetDescription>
-            Make changes to your profile here. Click save when you are done.
+            Upload product information here. Click save when you are done.
           </SheetDescription>
         </SheetHeader>
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
+            <Label htmlFor="name" className="text-left">
               Name
             </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+            <Input id="name" value="" className="col-span-3" />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
+            <Label htmlFor="username" className="text-left">
+              Description
             </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
+            <Input id="username" value="" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-left">
+              Amount
+            </Label>
+            <Input id="username" value="" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-left">
+              Location
+            </Label>
+            <Input id="username" value="" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-left">
+              Status
+            </Label>
+            <Select>
+              <SelectTrigger className="col-span-3">
+                <SelectValue placeholder="Select a status" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="availble">AVAILABLE</SelectItem>
+                <SelectItem value="checked_out">CHECKED_OUT</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-left">
+              Type
+            </Label>
+            <Select>
+              <SelectTrigger className="col-span-3">
+                <SelectValue placeholder="Select a type" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="patient_simulators">
+                  Patient Simulators
+                </SelectItem>
+                <SelectItem value="task_trainers">Task Trainers</SelectItem>
+                <SelectItem value="simulation_equipment">
+                  Simulation Equipment
+                </SelectItem>
+                <SelectItem value="medical_furniture">
+                  Medical Furniture
+                </SelectItem>
+                <SelectItem value="consmable_supplies">
+                  Consumable Supplies
+                </SelectItem>
+                <SelectItem value="nonconsmable_supplies">
+                  Non-consumable Supplies
+                </SelectItem>
+                <SelectItem value="computers">Computers</SelectItem>
+                <SelectItem value="office_supplies">Office Supplies</SelectItem>
+                <SelectItem value="other">Other</SelectItem>
+              </SelectContent>
+            </Select>
           </div>
         </div>
         <SheetFooter>
           <SheetClose asChild>
-            <Button type="submit">Save changes</Button>
+            <Button type="submit">Save product</Button>
           </SheetClose>
         </SheetFooter>
       </SheetContent>
