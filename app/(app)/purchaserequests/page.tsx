e35517b1cea477d64b66a0ae70a9941311b1b225
Nicholas Moreland <nmoreland18@outlook.com>
2023-07-12 02:25:25 -0400

    patch

---
+++ app/(app)/purchaserequests/page.tsx
@@ -0,0 +1,39 @@
+import { getUserSession } from "@/lib/auth";
+import { PurchaseRequest, columns } from "./columns";
+import { DataTable } from "./data-table";
+
+async function getData(): Promise<PurchaseRequest[]> {
+  const response = await fetch(
+    "https://shim-ventare.vercel.app/api/purchaserequests", {
+      next: {revalidate: 60},
+    });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch purchase requests.");
+  }
+
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
+export default async function PurchaseRequestPage() {
+  const data = await getData();
+  const session = await getUserSession();
+
+  return (
+<div className="container mx-auto py-10">
+      {session && (
+        <div className="bg-green-50 dark:bg-gray-800 border border-green-400 p-4 rounded-lg mb-4">
+          <div className="flex justify-between">
+            <p className="text-green-800 dark:text-green-400 text-md">
+              {JSON.stringify(session)}
+            </p>
+          </div>
+        </div>
+      )}
+
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
+}
