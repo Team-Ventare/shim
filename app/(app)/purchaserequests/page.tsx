86b4fbd405e9e300d989fbd62353033b24a6053c
Henry <115604958+kniatos1337@users.noreply.github.com>
2023-07-12 06:23:55 -0500

    [shadcn] REQUESTS TABLE -need to hook up to prisma -currently using mock data -need to auth headers

--- app/(app)/purchaserequests/page.tsx
+++ app/(app)/purchaserequests/page.tsx
@@ -1,38 +1,65 @@
-import { getUserSession } from "@/lib/auth";
-import { PurchaseRequest, columns } from "./columns";
-import { DataTable } from "./data-table";
+import { promises as fs } from "fs"
+import path from "path"
+import { Metadata } from "next"
+import Image from "next/image"
+import { z } from "zod"
 
-async function getData(): Promise<PurchaseRequest[]> {
-  const response = await fetch(
-    "https://shim-ventare.vercel.app/api/purchaserequests", {
-      next: {revalidate: 60},
-    });
+import { columns } from "./columns"
+import { DataTable } from "./data-table"
+import { UserNav } from "@/components/purchaserequest/user-nav"
+import { taskSchema } from "@/components/purchaserequest/data/schema"
 
-  if (!response.ok) {
-    throw new Error("Failed to fetch purchase requests.");
-  }
-
-  const data = await response.json();
-  return data;
+export const metadata: Metadata = {
+  title: "Tasks",
+  description: "A task and issue tracker build using Tanstack Table.",
 }
 
-export default async function PurchaseRequestPage() {
-  const data = await getData();
-  const session = await getUserSession();
+// Simulate a database read for tasks.
+async function getTasks() {
+  const data = await fs.readFile(
+    path.join(process.cwd(), "components/purchaserequest/data/tasks.json")
+  )
+
+  const tasks = JSON.parse(data.toString())
+
+  return z.array(taskSchema).parse(tasks)
+}
+
+export default async function TaskPage() {
+  const tasks = await getTasks()
 
   return (
-<div className="container mx-auto py-10">
-      {session && (
-        <div className="bg-green-50 dark:bg-gray-800 border border-green-400 p-4 rounded-lg mb-4">
-          <div className="flex justify-between">
-            <p className="text-green-800 dark:text-green-400 text-md">
-              {JSON.stringify(session)}
+    <>
+      <div className="md:hidden">
+        <Image
+          src="/examples/tasks-light.png"
+          width={1280}
+          height={998}
+          alt="Playground"
+          className="block dark:hidden"
+        />
+        <Image
+          src="/examples/tasks-dark.png"
+          width={1280}
+          height={998}
+          alt="Playground"
+          className="hidden dark:block"
+        />
+      </div>
+      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+        <div className="flex items-center justify-between space-y-2">
+          <div>
+            <h2 className="text-2xl font-bold tracking-tight">***ADMIN ACCESS ONLY***</h2>
+            <p className="text-muted-foreground">
+              Displaying history of all purchase requests!
             </p>
           </div>
+          <div className="flex items-center space-x-2">
+            <UserNav />
+          </div>
         </div>
-      )}
-
-      <DataTable columns={columns} data={data} />
-    </div>
-  );
+        <DataTable data={tasks} columns={columns} />
+      </div>
+    </>
+  )
 }
