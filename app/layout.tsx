11e2f319180183b655f313c5a0ad3ac01e292628
nicholasmoreland <nmoreland18@outlook.com>
2023-07-23 02:42:58 -0500

    body error patch

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,9 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Providers>{children}</Providers>
-      </body>
+      <Providers>{children}</Providers>
     </html>
   );
 }
