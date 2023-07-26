b56f47eb5351528a8dafe2b00ef4519e4e512425
Nicholas Moreland <nmoreland18@outlook.com>
2023-07-26 12:28:27 -0400

    prod patch

---
+++ app/(app)/suppliers/page.tsx
@@ -0,0 +1,30 @@
+import { getUserSession } from "@/lib/auth";
+import { Supplier, columns } from "./columns";
+import { DataTable } from "./data-table";
+
+async function getData(): Promise<Supplier[]> {
+  const response = await fetch(
+    "https://shim-ventare.vercel.app/api/suppliers",
+    {
+      cache: "no-store",
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch suppliers.");
+  }
+
+  const data = await response.json();
+  return data;
+}
+
+export default async function SupplierPage() {
+  const data = await getData();
+  const session = await getUserSession();
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
+}
