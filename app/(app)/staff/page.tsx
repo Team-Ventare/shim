9aab9e634f019f33d5d1a81314e23ac820732ac2
3nombo <enobong.mbosowo@gmail.com>
2023-07-26 12:23:00 -0500

    minor case changes to online, offline status handler

--- app/(app)/staff/page.tsx
+++ app/(app)/staff/page.tsx
@@ -53,18 +53,18 @@ export default function StaffPage() {
             <div className="hidden sm:flex sm:flex-col sm:items-end">
               <p className="text-sm leading-6 text-gray-900">{person.role}</p>
               {person.lastSeen ? (
-                <p className="mt-1 text-xs leading-5 text-gray-500">
-                  Last seen{" "}
-                  <time dateTime={person.lastSeen}>
-                    {person.lastSeen}
-                  </time>
-                </p>
+                <div className="mt-1 flex items-center gap-x-1.5">
+                <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+                  <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                </div>
+                <p className="text-xs leading-5 text-gray-500">Online</p>
+              </div>
               ) : (
                 <div className="mt-1 flex items-center gap-x-1.5">
-                  <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-                    <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                  <div className="flex-none rounded-full bg-red-500/20 p-1">
+                    <div className="h-1.5 w-1.5 rounded-full bg-red-500" />
                   </div>
-                  <p className="text-xs leading-5 text-gray-500">Online</p>
+                  <p className="text-xs leading-5 text-gray-500">Offline</p>
                 </div>
               )}
             </div>
