6d13f1432fa2cd8e30e15df873ea09f8cc45d027
Nicholas Moreland <nmoreland18@outlook.com>
2023-09-10 22:04:59 -0500

    ui redesign for 404 product pages

--- app/(app)/products/[id]/error.tsx
+++ app/(app)/products/[id]/error.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Error({
   error,
   reset,
@@ -8,11 +11,21 @@ export default function Error({
   reset: () => void;
 }) {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <main className="grid place-items-center px-6 py-24 sm:py-32 lg:px-8 h-screen">
       <div className="text-center">
-        <h2>Something went wrong!</h2>
-        <button onClick={() => reset()}>Try again</button>
+        <p className="text-base font-semibold text-indigo-600">404</p>
+        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+          Product not found
+        </h1>
+        <p className="mt-6 text-base leading-7 text-gray-600">
+          Sorry, we couldnt find the product you were looking for.
+        </p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Button asChild>
+            <Link href="/products">Go Back</Link>
+          </Button>
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
