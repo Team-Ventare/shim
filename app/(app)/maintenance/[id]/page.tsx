cf7c5fc709e5a5d5963a75cb150eb6fdaf7b406e
Henry <115604958+kniatos1337@users.noreply.github.com>
2023-11-02 18:41:32 -0500

    [initial] pdf view

--- app/(app)/maintenance/[id]/page.tsx
+++ app/(app)/maintenance/[id]/page.tsx
@@ -182,6 +182,13 @@ import {
                 </AccordionContent>
               </AccordionItem>
             </Accordion>
+
+            <object data="http://africau.edu/images/default/sample.pdf" type="application/pdf" width="100%" height="100%">
+              <p>Alternative text - include a link <a href="http://africau.edu/images/default/sample.pdf">to the PDF!</a></p>
+
+            </object>
+
+
           </div>
         </div>
       </div>
